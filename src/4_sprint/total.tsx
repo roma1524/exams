5220e0722e99de2042412395a6686f5e4fba656c
roma1524 <kulikr.js@mail.ru>
2025-04-02 00:20:41 +0300

    total exam is done

--- src/4_sprint/total.tsx
+++ src/4_sprint/total.tsx
@@ -384,3 +384,590 @@
 // // отображалась странице с информацией о друге.
 // // В качестве ответа укажите исправленную строку кода.
 // <Route path={"friend/:id"} element={<Profile />} />
+
+
+
+
+// import { configureStore, createSlice } from "@reduxjs/toolkit"
+// import { createRoot } from "react-dom/client"
+// import { Provider, useDispatch, useSelector } from "react-redux"
+//
+// // slice
+// const slice = createSlice({
+//   name: "playlist",
+//   initialState: {
+//     albums: [
+//       {
+//         id: 1,
+//         title: "Album 1",
+//         songs: [
+//           { id: 1, title: "Song 1-1" },
+//           { id: 2, title: "Song 1-2" },
+//         ],
+//       },
+//       {
+//         id: 2,
+//         title: "Album 2",
+//         songs: [
+//           { id: 3, title: "Song 2-1" },
+//           { id: 4, title: "Song 2-2" },
+//         ],
+//       },
+//     ],
+//   },
+//   reducers: {
+//     removeLastSongFromAlbum: (state, action) => {
+//       console.log(action.payload)
+//       const elArr = state.albums.find(item => item.id === action.payload)
+//       console.log(elArr)
+//       if (elArr) {
+//         elArr.songs.pop()
+//       }
+//       return state
+//     },
+//   },
+//   selectors: {
+//     selectAlbums: (state) => state.albums,
+//   },
+// })
+//
+// const { removeLastSongFromAlbum } = slice.actions
+// const { selectAlbums } = slice.selectors
+//
+// // App.tsx
+// const App = () => {
+//   const albums = useAppSelector(selectAlbums)
+//   const dispatch = useAppDispatch()
+//
+//   const removeLastSong = (albumId: number) => {
+//     dispatch(removeLastSongFromAlbum(albumId))
+//   }
+//
+//   return (
+//     <>
+//       {albums.map((album) => (
+//         <div key={album.id}>
+//           <h3>{album.title}</h3>
+//           <button onClick={() => removeLastSong(album.id)}>Remove Last Song</button>
+//           <ul>
+//             {album.songs.map((song) => (
+//               <li key={song.id}>{song.title}</li>
+//             ))}
+//           </ul>
+//         </div>
+//       ))}
+//     </>
+//   )
+// }
+//
+// // store.ts
+// const store = configureStore({
+//   reducer: {
+//     playlist: slice.reducer,
+//   },
+// })
+//
+// type RootState = ReturnType<typeof store.getState>
+// type AppDispatch = typeof store.dispatch
+// const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+// const useAppSelector = useSelector.withTypes<RootState>()
+//
+// // main.ts
+// createRoot(document.getElementById("root")!).render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+// )
+//
+// // 📜 Описание:
+// // Плейлист разделён на альбомы, и каждая кнопка Remove Last Song должна удалять последнюю песню из соответствующего альбома.
+//
+// // 🪛 Задача:
+// // Перепишите изменение стейта таким образом, чтобы описание выше выполнялось
+// // В качестве ответа укажите исправленный код написанный вместо return state.
+// // ❗Изменение стейта должно быть написано мутабельным образом.
+// // ❗Не используйте деструктуризацию action.payload (const {id} = action.payload)
+// // ❗Не создавайте переменные из action.payload (const id = action.payload.id)
+// const album = state.albums.find(a => a.id === action.payload)
+// if (album && album.songs.length > 0) {
+//   album.songs.pop()
+// }
+
+
+// import { configureStore, createSlice } from "@reduxjs/toolkit"
+// import { createRoot } from "react-dom/client"
+// import { Provider, useDispatch, useSelector } from "react-redux"
+//
+// // slice
+// const slice = createSlice({
+//   name: "products",
+//   initialState: [
+//     { id: 1, name: "Laptop", inStock: true, price: 1500 },
+//     { id: 2, name: "Smartphone", inStock: false, price: 800 },
+//     { id: 3, name: "Tablet", inStock: true, price: 600 },
+//   ],
+//   reducers: {
+//     applyDiscount: (state, action) => {
+//       const discount = action.payload / 100;
+//       state.forEach((el) => {
+//         el.price = el.price * (1 - discount);
+//       })
+//     },
+//   },
+//   selectors: {
+//     selectProducts: (state) => state,
+//   },
+// })
+//
+// const { applyDiscount } = slice.actions
+// const { selectProducts } = slice.selectors
+//
+// // App.tsx
+// const App = () => {
+//   const products = useAppSelector(selectProducts)
+//   const dispatch = useAppDispatch()
+//
+//   const handleDiscount = (discount: number) => {
+//     dispatch(applyDiscount(discount))
+//   }
+//
+//   return (
+//     <div>
+//       <button onClick={() => handleDiscount(10)}>10% Discount</button>
+//       <button onClick={() => handleDiscount(30)}>30% Discount</button>
+//       <button onClick={() => handleDiscount(50)}>50% Discount</button>
+//       <ul>
+//         {products.map((product) => (
+//           <li key={product.id}>
+//             <span>
+//               {product.name} ({product.inStock ? "In Stock" : "Out of Stock"}) - ${product.price.toFixed(2)}
+//             </span>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   )
+// }
+//
+// // store.ts
+// const store = configureStore({
+//   reducer: {
+//     products: slice.reducer,
+//   },
+// })
+//
+// type RootState = ReturnType<typeof store.getState>
+// type AppDispatch = typeof store.dispatch
+// const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+// const useAppSelector = useSelector.withTypes<RootState>()
+//
+// // main.ts
+// createRoot(document.getElementById("root")!).render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+// )
+//
+// // 📜 Описание:
+// // При нажатии на кнопки с 10%, 30% или 50% скидками цены всех продуктов должны уменьшиться на
+// // указанный процент.
+//
+// // 🪛 Задача:
+// // Перепишите изменение стейта так, чтобы цена каждого продукта уменьшалась на указанный процент.
+// // В качестве ответа укажите исправленный код написанный вместо return state.
+// // ❗Операция должна быть реализована мутабельным образом.
+//
+// const discount = action.payload / 100;
+// state.forEach((el) => {
+//   el.price = el.price * (1 - discount);
+// })
+
+// import { useEffect } from "react"
+// import { createRoot } from "react-dom/client"
+// import { Provider, useDispatch, useSelector } from "react-redux"
+// import axios from "axios"
+// import { asyncThunkCreator, buildCreateSlice, configureStore, Dispatch } from "@reduxjs/toolkit"
+//
+// // Types
+// type Todolist = {
+//   id: string
+//   title: string
+//   order: number
+//   createdAt: string
+//   updatedAt: string
+//   completed: boolean
+// }
+//
+// type User = {
+//   id: string
+//   name: string
+//   age: number
+// }
+//
+// type UsersResponse = {
+//   items: User[]
+//   totalCount: number
+// }
+//
+// // API
+// const instance = axios.create({ baseURL: "https://exams-frontend.kimitsu.it-incubator.io/api/" })
+//
+// const api = {
+//   getTodos() {
+//     return instance.get<Todolist[]>("todo")
+//   },
+//   getUsers() {
+//     return instance.get<UsersResponse>("user")
+//   },
+// }
+//
+// // Slice
+// const createAppSlice = buildCreateSlice({ creators: { asyncThunk: asyncThunkCreator } })
+//
+// const slice = createAppSlice({
+//   name: "app",
+//   initialState: {
+//     todolists: [] as Todolist[],
+//     users: [] as User[],
+//     error: null as string | null,
+//   },
+//   selectors: {
+//     selectTodolists: (state) => state.todolists,
+//     selectUsers: (state) => state.users,
+//     selectError: (state) => state.error,
+//   },
+//   reducers: (create) => ({
+//     setError: create.reducer<{ error: string | null }>((state, action) => {
+//       state.error = action.payload.error
+//     }),
+//     fetchTodolists: create.asyncThunk(
+//       async (_arg, { dispatch, rejectWithValue }) => {
+//         try {
+//           const res = await api.getTodos()
+//           return { todolists: res.data }
+//         } catch (error: any) {
+//           handleErrors(dispatch, error.message)
+//           return rejectWithValue(null)
+//         }
+//       },
+//       {
+//         fulfilled: (state, action) => {
+//           state.todolists = action.payload.todolists
+//         },
+//       },
+//     ),
+//     fetchUsers: create.asyncThunk(
+//       async (_arg, { dispatch, rejectWithValue }) => {
+//         try {
+//           const res = await api.getUsers()
+//           return { users: res.data.items }
+//         } catch (error: any) {
+//           handleErrors(dispatch, error.message)
+//           return rejectWithValue(null)
+//         }
+//       },
+//       {
+//         fulfilled: (state, action) => {
+//           state.users = action.payload.users
+//         },
+//       },
+//     ),
+//   }),
+// })
+//
+// const appReducer = slice.reducer
+// const { setError, fetchTodolists, fetchUsers } = slice.actions
+// const { selectTodolists, selectUsers, selectError } = slice.selectors
+//
+// // Utils functions
+// const handleErrors = (dispatch: AppDispatch, errorMessage: string) => {
+//   dispatch(setError({ error: errorMessage }))
+// }
+//
+// const App = () => {
+//   return (
+//     <>
+//       <h1>✅Todos & 🙂Users</h1>
+//       <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+//         <Todos />
+//         <Users />
+//       </div>
+//     </>
+//   )
+// }
+//
+// const Todos = () => {
+//   const dispatch = useAppDispatch()
+//   const todolists = useAppSelector(selectTodolists)
+//   const error = useAppSelector(selectError)
+//
+//   useEffect(() => {
+//     dispatch(fetchTodolists())
+//   }, [])
+//
+//   return (
+//     <div>
+//       <h2>✅ Список тудулистов</h2>
+//       {!!error && <h2 style={{ color: "red" }}>{error}</h2>}
+//       {todolists.map((todolist) => (
+//         <div style={todolist.completed ? { color: "grey" } : {}} key={todolist.id}>
+//           <input type="checkbox" checked={todolist.completed} />
+//           <b>Описание</b>: {todolist.title}
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
+//
+// const Users = () => {
+//   const dispatch = useAppDispatch()
+//   const users = useAppSelector(selectUsers)
+//   const error = useAppSelector(selectError)
+//
+//   useEffect(() => {
+//     dispatch(fetchUsers())
+//   }, [])
+//
+//   return (
+//     <div>
+//       <h2>🙂 Список юзеров</h2>
+//       {!!error && <h2 style={{ color: "red" }}>{error}</h2>}
+//       <div>
+//         {users.map((user) => (
+//           <div key={user.id}>
+//             <b>name</b>:{user.name} - <b>age</b>:{user.age}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   )
+// }
+//
+// // Store
+// const store = configureStore({
+//   reducer: {
+//     [slice.name]: appReducer,
+//   },
+// })
+//
+// type RootState = ReturnType<typeof store.getState>
+// type AppDispatch = typeof store.dispatch
+// const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+// const useAppSelector = useSelector.withTypes<RootState>()
+//
+// createRoot(document.getElementById("root")!).render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+// )
+// // 📜 Описание:
+// // Откройте network и вы увидите что запросы за тудулистами и пользователями падают с ошибками,
+// // но пользователе не видит ошибок, потому что утилитная функция handleErrors написана неверно.
+// // Ваша задача дописать функцию handleErrors, чтобы пользователь на экране увидел ошибки:
+// // 'Request failed with status code 404'
+// //❗ Код фиксить не нужно.
+// //❗ Тип any типизации указывать запрещено
+// // В качестве ответа укажите полностью написанную функцию
+//
+// // Пример ответа:
+// // const handleErrors = () => {
+// //   console.log("error")
+// // }
+// const handleErrors = (dispatch: AppDispatch, errorMessage: string) => {
+//   dispatch(setError({ error: errorMessage }))
+// }
+
+
+// import { configureStore, createSlice } from "@reduxjs/toolkit"
+// import { createRoot } from "react-dom/client"
+// import { Provider, useDispatch, useSelector } from "react-redux"
+//
+// // oxygenCounter slice
+// const oxygenSlice = createSlice({
+//   name: "oxygenCounter",
+//   initialState: {
+//     percent: 21,
+//   },
+//   reducers: {
+//     increase: (state) => {
+//       state.percent += 1
+//     },
+//   },
+//   selectors: {
+//     selectPercent: (state) => state.percent,
+//   },
+// })
+// const { increase } = oxygenSlice.actions
+// const { selectPercent } = oxygenSlice.selectors
+//
+// // temperature slice
+// const temperatureSlice = createSlice({
+//   name: "temperatureCounter",
+//   initialState: {
+//     celsius: 20,
+//   },
+//   reducers: {},
+//   selectors: {
+//     selectCelsius: (state) => state.celsius,
+//   },
+//   extraReducers: (builder) => {
+//     builder.addCase(increase, (state) => {
+//       state.celsius += 2
+//     })
+//   }
+// })
+//
+// const { selectCelsius } = temperatureSlice.selectors
+//
+// // App.tsx
+// const App = () => {
+//   const oxygen = useAppSelector(selectPercent)
+//   const temperature = useAppSelector(selectCelsius)
+//   const dispatch = useAppDispatch()
+//
+//   return (
+//     <>
+//       <button onClick={() => dispatch(increase())}>Add Oxygen</button>
+//       <div>Oxygen: {oxygen}%</div>
+//       <hr />
+//       <div>Temperature: {temperature}°C</div>
+//     </>
+//   )
+// }
+//
+// // store.ts
+// const store = configureStore({
+//   reducer: {
+//     oxygenCounter: oxygenSlice.reducer,
+//     temperatureCounter: temperatureSlice.reducer,
+//   },
+// })
+//
+// type RootState = ReturnType<typeof store.getState>
+// type AppDispatch = typeof store.dispatch
+// const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+// const useAppSelector = useSelector.withTypes<RootState>()
+//
+// // main.ts
+// createRoot(document.getElementById("root")!).render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+// )
+//
+// // 📜 Описание:
+// // У вас есть два счетчика: для уровня кислорода (%) и температуры (°C).
+// // При нажатии на кнопку Add Oxygen увеличивается уровень кислорода.
+//
+// // 🪛 Задача:
+// // Реализуйте следующую задачу:
+// // При нажатии на кнопку Add Oxygen помимо увеличения уровня кислорода
+// // реализуйте увеличении температуры на 2°C
+//
+// // В качестве ответа укажите добавленный вами код
+// // ❗Операция должна быть реализована мутабельным образом.
+// // 💡Подсказка. Используйте extraReducers
+//
+// extraReducers: (builder) => {
+//   builder.addCase(increase, (state) => {
+//     state.celsius += 2
+//   })
+// }
+
+
+// import { CSSProperties } from "react"
+// import { createRoot } from "react-dom/client"
+// import { BrowserRouter, NavLink, Route, Routes } from "react-router"
+//
+// const footer: CSSProperties = {
+//   padding: 10,
+//   background: "lightslategrey",
+// }
+//
+// const link: CSSProperties = {
+//   color: "lightsalmon",
+//   fontSize: 20,
+// }
+//
+// const Dragons = () => {
+//   return <h1>🐲 🐲 🐲</h1>
+// }
+//
+// const Cats = () => {
+//   return <h1>😺 🐱 😼</h1>
+// }
+//
+// const Dogs = () => {
+//   return <h1>🐶 🐶 🐶</h1>
+// }
+//
+// const PageNotFound = () => {
+//   return <h1>4️⃣0️⃣4️⃣</h1>
+// }
+//
+// const Menu = () => {
+//   return (
+//     <ul>
+//       <li>
+//         <NavLink to={Path.Dragons} style={link}>
+//           dragons
+//         </NavLink>
+//       </li>
+//       <li>
+//         <NavLink to={Path.Cats} style={link}>
+//           cats
+//         </NavLink>
+//       </li>
+//       <li>
+//         <NavLink to={Path.Dogs} style={link}>
+//           dogs
+//         </NavLink>
+//       </li>
+//     </ul>
+//   )
+// }
+//
+// // Routing
+// const Path = {
+//   Dragons: "dragons",
+//   Cats: "cats",
+//   Dogs: "dogs",
+// } as const
+//
+// const Routing = () => {
+//   return (
+//     <Routes>
+//       <Route path={"/"} element={<div></div>} />
+//       <Route path={Path.Dragons} element={<Dragons />} />
+//       <Route path={Path.Cats} element={<Cats />} />
+//       <Route path={Path.Dogs} element={<Dogs />} />
+//       <Route path={"*"} element={<PageNotFound />} />
+//     </Routes>
+//   )
+// }
+//
+// const App = () => {
+//   return (
+//     <>
+//       <Menu />
+//       <Routing />
+//       <footer style={footer}>
+//         <h2>Footer</h2>
+//       </footer>
+//     </>
+//   )
+// }
+//
+// createRoot(document.getElementById("root")!).render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+// )
+//
+// // 📜 Описание:
+// // Реализуйте Page not found
+//
+// // 🪛 Задача:
+// // Что нужно написать вместо {/*❗XXX */},
+// // чтобы при url `http://localhost:3000/fsdfdsf` и других не существующих страницах
+// // отработал роут Page not found
